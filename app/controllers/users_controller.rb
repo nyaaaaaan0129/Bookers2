c993390b56d4a7c58730c9534ca0cc01a5edac10
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-05-07 02:07:03 +0900

    Second commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def show
   	  @user = User.find(params[:id])
+  	  @books = Book.all
+  	  @book = Book.new
+
   end
+  def create
+        book = Book.new(book_params)
+        book.user_id = current_user.id
+        book.save
+        redirect_to books_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root  "homes#top"
+  get 'about' => 'homes#about'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only:[:show]
+  resources :users, only:[:show, :create]
   resources :books, only: [:new, :create, :index, :show]
+ 
 end
