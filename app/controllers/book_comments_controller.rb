f4dd935fa27d277ea2e201defe8ecbbb6d717dd7
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-05-18 14:42:55 +0900

    Submission

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,6 +8,25 @@ class BookCommentsController < ApplicationController
 		comment.save
 		redirect_to book_path(book)
 	end
+	def destroy
+        # book = Book.find(params[:book_id])
+        comment = BookComment.find(params[:book_id])
+        comment.destroy if comment.user_id == current_user.id
+        # comment = book.book_comments.find(params[:id])
+        # comment.destro
+        redirect_to book_path(comment.book_id)
+
+        # # ==========================================
+        # book = Book.find(params[:book_id])
+        # comment = BookComment.find(params[:book_comment_id]) # link_to book_comment_id
+        # if (book.id == comment.book_id) && (current_user.id == comment.user_id)
+        # 	comment.destroy
+        # end
+
+        # redirect_to book_path(book.id)
+    end
+
+
 	private
 	def book_comment_params
 		params.require(:book_comment).permit(:comment)
