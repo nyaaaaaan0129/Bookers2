0741047e2cba48a4a4c0d2784b590ced54ed6fcf
村田花穂 <muratahanaho@muratakasuinoMacBook-Air.local>
2020-05-21 18:40:02 +0900

    Submission for exercise4

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,33 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :books, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   validates :name, presence: true, length:{ minimum: 2, maximum: 20, message: '2文字以上20文字以内です'}
   validates :introduction, length:{ maximum: 50, message: '50文字以内です'}
+
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   get 'home/about', to: 'homes#about'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only:[:show, :create, :edit, :update, :index]
+  resources :users, only:[:show, :create, :edit, :update, :index] do
+  	member do
+  		get :following, :followers
+  	end
+  end
 
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
      resource :book_comments, only:[:create, :destroy]
      resource :favorites, only: [:create, :destroy]
   end
+  resources :relationships,       only: [:create, :destroy]
 end
